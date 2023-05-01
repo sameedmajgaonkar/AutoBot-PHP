e9c7ef502da3faf3bb7840bd3b356ef238470488
sameed1718 <sameed1718@gmail.com>
2023-05-01 15:49:00 +0530

    Second commit

--- chatbot.php
+++ chatbot.php
@@ -1,5 +1,4 @@
 <?php
-// Connect to the database
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,12 +6,10 @@ $dbname = "chatbot";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-// Look for a matching response in the database
 $sql = "SELECT * FROM chatbot_responses";
 $result = mysqli_query($conn, $sql);
 $fetch_data = mysqli_fetch_all($result, MYSQLI_ASSOC);
--- script.js
+++ script.js
@@ -1,47 +1,50 @@
-const messages = document.getElementById('messages');
-const input = document.getElementById('input');
-const send = document.getElementById('send');
+const messages = document.getElementById('messages')
+const input = document.getElementById('input')
+const send = document.getElementById('send')
+const chat_container = document.getElementById('chatbot')
 
-function sendMessage() {
-  const text = input.value.trim();
-  if (text.length === 0) return;
-
-  const message = document.createElement('div');
-  message.classList.add('message', 'user');
-  message.textContent = text;
-  messages.appendChild(message);
-
-  fetch('chatbot.php')
+function sendMessage(){
+    const text = input.value.trim() //remove extra spaces
+    if(text.length === 0){return}
+    const message = document.createElement('div')
+    message.classList.add('message', 'user')
+    message.textContent = text
+    messages.appendChild(message)
+    fetch('chatbot.php')
     .then(response => response.json())
     .then(data => {
-      const answer = findAnswer(data, text);
-      displayAnswer(answer);
+        const answer = findAnswer(data, text)
+        displayAnswer(answer)
     })
-    .catch(error => console.error(error));
-
-  input.value = '';
+    .catch(error => console.log(error))
+    input.value = ''
 }
 
-function findAnswer(data, text) {
-  for (let i = 0; i < data.length; i++) {
-    const question = data[i].question.toLowerCase();
-    if (text.toLowerCase().includes(question)) {
-      return data[i].answer;
+function findAnswer(data, text){
+    for(let i =0; i < data.length; i++){
+        const question = data[i].question.toLowerCase();
+        if(text.toLowerCase().includes(question)){
+            return data[i].answer
+        }
     }
-  }
-  return 'I\'m sorry, I don\'t understand. Please try again.';
+    return "Sorry I didn't Understand ! Please Try Again"
 }
 
-function displayAnswer(answer) {
-  const message = document.createElement('div');
-  message.classList.add('message', 'bot');
-  message.textContent = answer;
-  messages.appendChild(message);
+function displayAnswer(answer){
+    const message = document.createElement('div')
+    message.classList.add('message', 'bot')
+    message.textContent = answer
+    messages.appendChild(message)
+    const utterance = new SpeechSynthesisUtterance(answer)
+    utterance.voice = speechSynthesis.getVoices()[1]
+    speechSynthesis.speak(utterance)
+    chat_container.scrollTop = chat_container.scrollHeight
 }
 
-send.addEventListener('click', sendMessage);
-input.addEventListener('keydown', event => {
-  if (event.key === 'Enter') {
-    sendMessage();
-  }
-});
+
+send.addEventListener('click',sendMessage);
+input.addEventListener('keydown', event =>{
+    if(event.key == 'Enter'){
+        sendMessage()
+    }
+})
